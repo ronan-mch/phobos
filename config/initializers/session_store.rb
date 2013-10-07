cd2d56bcadf95e93ffa0e227ee0abd93d88a5e98
Ronan McHugh <mchugh.r@gmail.com>
2013-10-07 15:51:13 +0200

    keep session in cache to prevent api errors

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Phobos::Application.config.session_store :cookie_store, key: '_phobos_session'
+Phobos::Application.config.session_store :cache_store, key: '_phobos_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
