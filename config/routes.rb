cdf3f2165a141670ddfda60f516d5f5c675fb142
Ronan McHugh <mchugh.r@gmail.com>
2013-11-19 15:29:13 +0100

    make default side param = A

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Phobos::Application.routes.draw do
   match 'trafiklys/get_ip', to: 'trafiklys#get_ip'
   #if we don't get any parameters - show an error
   match 'trafiklys/lookUp', to: 'trafiklys#render_error'
-  match '/', to: 'search#journal_list'
+  match '/', to: 'search#journal_list', :id => 'A'
 
 
   Umlaut::Routes.new(self).draw
