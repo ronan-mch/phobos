32decd94fb954555bb18683b67c30b7256c8e205
Ronan McHugh <mchugh.r@gmail.com>
2013-11-25 11:21:34 +0100

    move locale handling into Application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+
+  before_filter :set_locale
+
+  # if no locale is set, will default to default_locale (i.e. en)
+  def set_locale
+    I18n.locale = params['umlaut.locale'.to_sym] || I18n.default_locale
+  end
+
+  # ensure locale is always included in any internal links
+  def default_url_options(options={})
+    { 'umlaut.locale'.to_sym => I18n.locale }
+  end
 end
