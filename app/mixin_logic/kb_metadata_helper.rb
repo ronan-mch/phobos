cb0d1df6adfabf05328f786c81ca1cdda1c23f68
Ronan McHugh <mchugh.r@gmail.com>
2013-11-25 11:14:54 +0100

    fix problem with finding title in metadata.

--- app/mixin_logic/kb_metadata_helper.rb
+++ app/mixin_logic/kb_metadata_helper.rb
@@ -15,4 +15,30 @@ module KbMetadataHelper
     issn || eissn || isbn || ''
   end
 
+  # pick title out of OpenURL referent from best element available,
+  # no normalization.
+  def raw_search_title(rft)
+    # Just make one call to create metadata hash
+    metadata = rft.metadata
+    title = nil
+    if rft.format == 'journal' && metadata['atitle']
+      title = metadata['atitle']
+    elsif rft.format == 'book'
+      title = metadata['btitle'] unless metadata['btitle'].blank?
+      title = metadata['title'] if title.blank?
+      # Well, if we don't know the format and we do have a title use that.
+      # This might happen if we only have an ISBN to start and then enhance.
+      # So should services like Amazon also enhance with a format, should
+      # we simplify this method to not worry about format so much, or do we
+      # keep this as is?
+    elsif metadata['btitle'] && metadata['btitle'].length > 0
+      title = metadata['btitle']
+    elsif metadata['title'] && metadata['title'].length > 0
+      title = metadata['title']
+    elsif metadata['jtitle'] && metadata['jtitle'].length > 0
+      title = metadata['jtitle']
+    end
+    title
+  end
+
 end
