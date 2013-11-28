9cb68906110c38db4793cddbbd753368ca12376f
Ronan McHugh <mchugh.r@gmail.com>
2013-11-28 10:02:42 +0100

    ensure typeahead is not called when search bar is missing. add trim to typeahead logic.

--- app/assets/javascripts/umlaut/search_autocomplete.js
+++ app/assets/javascripts/umlaut/search_autocomplete.js
@@ -1,15 +1,27 @@
 jQuery(document).ready(function($) {
 
-    //typeahead for our search bar
-    $('input.title_search').typeahead({
-        name: 'journals',
-        valueKey: 'title',
-        limit: 10,
-        remote: {
-            url: getRemoteUrl(),
-            replace: updateUrl
+    //add trim functionality to IE
+    if(typeof String.prototype.trim !== 'function') {
+        String.prototype.trim = function() {
+            return this.replace(/^\s+|\s+$/g, '');
         }
-    });
+    }
+
+
+    //typeahead for our search bar
+    var titleBar = $('input#journal_title');
+    if (titleBar.length > 0) {
+       titleBar.typeahead({
+            name: 'journals',
+            valueKey: 'title',
+            limit: 10,
+            remote: {
+                url: getRemoteUrl(),
+                replace: updateUrl
+            }
+        });
+    }
+
 
     //get the remot url for our typeahead based on the form's controller
     //with updated action and all form parameters smooshed on
@@ -24,7 +36,7 @@ jQuery(document).ready(function($) {
         searchVal = $('input.title_search').val();
         if (searchVal) {
             //if there's only one word, do a begins with search
-            if (searchVal.split(" ").length == 1) {
+            if (searchVal.trim().split(" ").length == 1) {
                 url = url.replace(/umlaut\.search_type=.+&/, 'umlaut.title_search_type=begins&');
             }
             return url + searchVal;
